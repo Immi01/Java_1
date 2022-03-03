f11ccd9406fe10d4a655aad64e4b2c121da07220
Immi01 <immanuel.ehe@gmx.at>
2022-03-03 16:27:26 +0100

    created and completed Loops1

---
+++ src/at/ImmanuelEhe/basics/Loops1.java
@@ -0,0 +1,17 @@
+package at.ImmanuelEhe.basics;
+
+public class Loops1 {
+
+    public static void main(String[] args) {
+
+        int Result = 0;
+
+        for (int i = 1; i <= 100; i++){
+            Result = Result + i;
+        }
+
+        System.out.println(Result);
+
+    }
+
+}
