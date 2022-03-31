c442284670fb51dcc7440872689e30e7e44f9fcd
Immi01 <immanuel.ehe@gmx.at>
2022-03-31 18:45:27 +0200

    completed Bankomat.java

--- src/at/ImmanuelEhe/Bankomat/Bankomat.java
+++ src/at/ImmanuelEhe/Bankomat/Bankomat.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 
 public class Bankomat {
 
+    static int input = 0;
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         boolean exit = false;
-        Scanner scanner = new Scanner(System.in);
 
         int balance = 0;
-        int input = 0;
 
         while(!exit){
 
+            System.out.println();
             System.out.println("Wählen sie eine Option aus:");
             System.out.println("geben sie 1 für eine Einzahlung ein.");
             System.out.println("2 für eine Auszahlung.");
@@ -22,62 +24,48 @@ public class Bankomat {
             System.out.println("4 um sich abzumelden.");
             System.out.println();
 
-            try{
-                input = scanner.nextInt();
-            } catch(InputMismatchException e) {
-                System.out.println("Bitte geben sie eine Zahl ein.");
-            }
+            input("Bitte geben sie eine der oben genannten Zahlen ein.", true);
 
             switch(input){
                 case 1:
 
-                    try{
-                        System.out.println();
-                        System.out.println("Bitte geben sie den Betrag ein, den Sie Einzahlen wollen.");
-                        System.out.println();
+                    System.out.println();
+                    System.out.println("Bitte geben sie den Betrag ein, den Sie Einzahlen wollen.");
+                    System.out.println();
+                    input("Bitte geben sie keine Kommastellen oder zu hohen Zahlen ein.");
 
-                        input = scanner.nextInt();
-
-                    } catch(InputMismatchException e) {
-                        System.out.println("Bitte geben sie eine Zahl ein.");
-                    }
 
                     balance += input;
                     System.out.println();
-                    System.out.println("Es wurden " + input + " Euro eingezahlt.");
+                    if(input == 1)
+                        System.out.println("Es wurde " + input + " Euro eingezahlt.");
+                    else
+                        System.out.println("Es wurden " + input + " Euro eingezahlt.");
                     System.out.println();
                     break;
 
                 case 2:
 
-                    try{
-                        System.out.println();
-                        System.out.println("Bitte geben sie den Betrag ein, den Sie sich Auszahlen lassen wollen.");
-                        System.out.println();
+                    System.out.println();
+                    System.out.println("Bitte geben sie den Betrag ein, den Sie sich Auszahlen lassen wollen.");
+                    System.out.println();
+                    input("Bitte geben sie keine Kommastellen oder zu hohen Zahlen ein.");
 
-                        input = scanner.nextInt();
 
-                    } catch(InputMismatchException e) {
-                        System.out.println("Bitte geben sie eine Zahl ein.");
-                    }
-
+                    System.out.println();
                     if(input <= balance) {
                         balance -= input;
-
-                        System.out.println();
                         System.out.println("Es wurden " + input + " Euro abgehoben.");
-                        System.out.println();
+
                     } else {
-                        System.out.println();
                         System.out.println("Sie können nicht mehr Geld abheben als sie besitzen.");
-                        System.out.println();
                     }
+                    System.out.println();
                     break;
 
                 case 3:
                     System.out.println();
                     System.out.println("Ihr derzeitiger Kontostand beträgt " + balance + " Euro");
-                    System.out.println();
                     break;
 
                 case 4:
@@ -93,11 +81,29 @@ public class Bankomat {
 
     }
 
-    static void input(){
-        try{
-            input = scanner.nextInt();
-        } catch(InputMismatchException e) {
-            System.out.println("Bitte geben sie eine Zahl ein.");
+    static void input (String errMessage){
+        input(errMessage, false);
+    }
+
+    static void input(String errMessage, boolean notPrintLast){
+
+        boolean Error = true;
+        int counter = 0;
+        int maxValue = 5;
+
+        while(Error && counter < maxValue) {
+            try {
+                input = scanner.nextInt();
+                Error = false;
+            } catch (InputMismatchException e) {
+                scanner.next();
+                if(counter != maxValue - 1 || !notPrintLast)
+                    System.out.println(errMessage);
+                Error = true;
+            }
+            counter++;
+            if (counter == maxValue && Error)
+                input = 0;
         }
     }
 
