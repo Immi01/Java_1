74439c287e71c428794f468d3589ecf092330ed2
Immi01 <immanuel.ehe@gmx.at>
2022-06-16 12:10:02 +0200

    continued on the Arrays chapter, completed the cross sum tasks

---
+++ src/at/ImmanuelEhe/Arrays/CrossSum1.java
@@ -0,0 +1,22 @@
+package at.ImmanuelEhe.Arrays;
+
+public class CrossSum1 {
+    public static void main(String[] args) {
+        //Gib alle Zahlen zwischen 0 und 1000 aus, welche eine Quersumme von 15 haben.
+        //Beispiel: die Quersumme von 253 ist 2 + 5 + 3 = 10
+
+        for (int i = 0; i <= 1000; i++) {
+            int crossSum = 0;
+            int tmp = i;
+            while (tmp > 0) {
+                crossSum += tmp % 10;
+                tmp /= 10;
+            }
+            if (crossSum == 15){
+                System.out.println(i);
+            }
+        }
+
+    }
+
+}
