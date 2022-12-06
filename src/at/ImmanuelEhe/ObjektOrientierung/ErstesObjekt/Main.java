ddef5896468765b64939632c703e8a8dfdc80c34
Immi01 <immanuel.ehe@gmx.at>
2022-12-06 10:50:21 +0100

    completed Objectorientation up until 04.09

--- src/at/ImmanuelEhe/ObjektOrientierung/ErstesObjekt/Main.java
+++ src/at/ImmanuelEhe/ObjektOrientierung/ErstesObjekt/Main.java
@@ -1,9 +1,6 @@
 package at.ImmanuelEhe.ObjektOrientierung.ErstesObjekt;
 
-import at.ImmanuelEhe.ObjektOrientierung.ErstesObjekt.objects.Car;
-import at.ImmanuelEhe.ObjektOrientierung.ErstesObjekt.objects.Engine;
-import at.ImmanuelEhe.ObjektOrientierung.ErstesObjekt.objects.Tank;
-import at.ImmanuelEhe.ObjektOrientierung.ErstesObjekt.objects.Wheel;
+import at.ImmanuelEhe.ObjektOrientierung.ErstesObjekt.objects.*;
 
 public class Main {
 
@@ -12,24 +9,30 @@ public class Main {
 
         Wheel wheel1 = new Wheel(205, 55, Wheel.Condition.GOOD);
         Wheel wheel2 = new Wheel(205, 55, Wheel.Condition.GOOD);
-        Wheel wheel3 = new Wheel(205, 55, Wheel.Condition.GOOD);
-        Wheel wheel4 = new Wheel(205, 55, Wheel.Condition.GOOD);
+        Wheel wheel3 = new Wheel(210, 55, Wheel.Condition.OK);
+        Wheel wheel4 = new Wheel(210, 55, Wheel.Condition.OK);
+
+        Backmirror backmirror1 = new Backmirror(10, 20);
+        Backmirror backmirror2 = new Backmirror(10, 20);
 
         Wheel[] wheels1 = {wheel1, wheel2, wheel3, wheel4};
+        Backmirror[] backmirrors1 = {backmirror1, backmirror2};
+
+
 
         Engine c1Engine = new Engine(false, 30);
         Tank c1Tank = new Tank(80, 70, Tank.TYPE.DIESEL);
-        Car c1 = new Car(c1Engine, c1Tank,7, "Audi", "A1234");
+        Car c1 = new Car(c1Engine, c1Tank, wheels1, backmirrors1,  7, "Audi", "A1234");
 
 
         Engine c2Engine = new Engine(false, 45);
         Tank c2Tank = new Tank(110, 20, Tank.TYPE.PETROL);
-        Car c2 = new Car(c2Engine, c2Tank,6, "Mercedes", "M1234");
+        Car c2 = new Car(c2Engine, c2Tank, wheels1, backmirrors1,6, "Mercedes", "M1234");
 
 
         Engine c3Engine = new Engine(false, 20);
         Tank c3Tank = new Tank(50, 40, Tank.TYPE.DIESEL);
-        Car c3 = new Car(c3Engine, c3Tank,17, "Opel", "1234O");
+        Car c3 = new Car(c3Engine, c3Tank, wheels1, backmirrors1,17, "Opel", "1234O");
 
         System.out.println(c3.getSerialNumber());
 
