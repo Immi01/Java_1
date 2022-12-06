f51d44381c87274d7ee988c046cad2e91fbfd6c9
Immi01 <immanuel.ehe@gmx.at>
2022-12-06 10:05:48 +0100

    completed Objectorientation up until 04.08 and started with 4.09

--- src/at/ImmanuelEhe/ObjektOrientierung/ErstesObjekt/Main.java
+++ src/at/ImmanuelEhe/ObjektOrientierung/ErstesObjekt/Main.java
@@ -1,38 +1,46 @@
 package at.ImmanuelEhe.ObjektOrientierung.ErstesObjekt;
 
 import at.ImmanuelEhe.ObjektOrientierung.ErstesObjekt.objects.Car;
+import at.ImmanuelEhe.ObjektOrientierung.ErstesObjekt.objects.Engine;
+import at.ImmanuelEhe.ObjektOrientierung.ErstesObjekt.objects.Tank;
+import at.ImmanuelEhe.ObjektOrientierung.ErstesObjekt.objects.Wheel;
 
 public class Main {
 
     public static void main(String[] args) {
         int a = 7;
 
-        /*Car c1 = new Car();
-        c1.brand = "Audi";
-        c1.fuelConsumption = 7;
-        c1.serialNumber = "A1234";
-        c1.fuelAmount = 70;
-        c1.tankVolume = 80;
+        Wheel wheel1 = new Wheel(205, 55, Wheel.Condition.GOOD);
+        Wheel wheel2 = new Wheel(205, 55, Wheel.Condition.GOOD);
+        Wheel wheel3 = new Wheel(205, 55, Wheel.Condition.GOOD);
+        Wheel wheel4 = new Wheel(205, 55, Wheel.Condition.GOOD);
 
-        Car c2 = new Car();
-        c2.brand = "Mercedes";
-        c2.fuelConsumption = 6;
-        c2.serialNumber = "M1234";
-        c2.fuelAmount = 20;
-        c2.tankVolume = 110;*/
+        Wheel[] wheels1 = {wheel1, wheel2, wheel3, wheel4};
 
-        Car c3 = new Car(17, "Opel", "1234O");
+        Engine c1Engine = new Engine(false, 30);
+        Tank c1Tank = new Tank(80, 70, Tank.TYPE.DIESEL);
+        Car c1 = new Car(c1Engine, c1Tank,7, "Audi", "A1234");
 
-        System.out.println(c3.serialNumber);
 
-        /*
+        Engine c2Engine = new Engine(false, 45);
+        Tank c2Tank = new Tank(110, 20, Tank.TYPE.PETROL);
+        Car c2 = new Car(c2Engine, c2Tank,6, "Mercedes", "M1234");
+
+
+        Engine c3Engine = new Engine(false, 20);
+        Tank c3Tank = new Tank(50, 40, Tank.TYPE.DIESEL);
+        Car c3 = new Car(c3Engine, c3Tank,17, "Opel", "1234O");
+
+        System.out.println(c3.getSerialNumber());
+
+
         c2.pressBreak();
 
         c2.turboBoost();
 
         c2.honk(2);
 
-        System.out.println(c2.getRemainingRange())*/
+        System.out.println(c2.getRemainingRange());
 
     }
 
--- src/at/ImmanuelEhe/ObjektOrientierung/ErstesObjekt/objects/Car.java
+++ src/at/ImmanuelEhe/ObjektOrientierung/ErstesObjekt/objects/Car.java
@@ -3,23 +3,30 @@ package at.ImmanuelEhe.ObjektOrientierung.ErstesObjekt.objects;
 public class Car {
     // Instanz / Gedächtnisvariablen
 
-    //dont do that later
-    public int fuelConsumption;
-    public int fuelAmount;
-    public int tankVolume;
-    public String brand;
-    public String serialNumber;
+    private int fuelConsumption;
+    private String brand;
+    private String serialNumber;
     private String color;
+    private final Engine engine;
+    private final Tank tank;
+    private Backmirror[] backmirror = new Backmirror[2];
+    private Wheel[] wheel = new Wheel[4];
 
 
-    public Car(int fuelConsumption, String brand, String serialNumber){
+    public Car(Engine engine, Tank tank, Wheel[] wheel, Backmirror[] backmirror, int fuelConsumption, String brand, String serialNumber){
+        this.engine = engine;
+        this.tank = tank;
+        this.wheel = wheel;
+        this.backmirror = backmirror;
         this.fuelConsumption = fuelConsumption;
         this.brand = brand;
         this.serialNumber = serialNumber;
     }
 
-    public void drive() {
-        this.fuelAmount = this.fuelAmount - fuelConsumption;
+    public void drive(int speed) {
+        tank.setTankVolume(tank.getFuelAmount() - fuelConsumption);
+        engine.setOn(true);
+        engine.setSpeed(speed);
         System.out.println("I am driving");
     }
 
@@ -28,7 +35,7 @@ public class Car {
     }
 
     public void turboBoost() {
-        if (this.fuelAmount > tankVolume/10)
+        if (tank.getFuelAmount() > tank.getTankVolume()/10)
             System.out.println("SuperBoostMode");
         else
             System.out.println("Not enough fuel to go to Superboost");
@@ -41,8 +48,42 @@ public class Car {
         }
     }
 
-    public int getRemainingRange() {
-        return this.fuelAmount/fuelConsumption*100;
+
+
+    public void setBrand(String brand) {
+        this.brand = brand;
     }
 
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setFuelConsumption(int fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public int getRemainingRange() {
+        return tank.getFuelAmount()/fuelConsumption*100;
+    }
+
+    public int getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
 }
